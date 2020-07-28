a15697eb729c5df7c4b9d7e82bc12b512e965a3a
abdulwasey18 <abdulwasey020@gmail.com>
2020-07-28 15:43:36 +0530

    added script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnSubmit = document.getElementById("btnSubmit");
 const temp = document.getElementById("temp");
 
 btnSubmit.onclick = function() {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=4af9e044737f83dd0831f27f696d9b41`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=4af9e044737f83dd0831f27f696d9b41`
 
     fetch(url)
         .then(function(res) {
